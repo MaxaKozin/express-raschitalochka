6b925fab4f7a33b5d2c8f705a66eba2e8a7c9028
YaroslavKuzero <49403383+YaroslavKuzero@users.noreply.github.com>
2021-01-05 12:14:14 +0200

    deploy to heroku

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ const serverInit = async () => {
     next(err);
   });
 
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"))
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  }
+
   app.listen(PORT, () => {
     console.log("Server is working on port: ", PORT);
   });
